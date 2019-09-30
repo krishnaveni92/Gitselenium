a1be3c302a7b7acd7b5f35b388aeb311fd3beee1
krishnaveni92 <krishnaveni.donthula92@gmail.com>
2019-09-30 14:48:12 +0530

    remove conflicts

--- src/com/git/webtable/GitWebtable.java
+++ src/com/git/webtable/GitWebtable.java
@@ -3,7 +3,7 @@ package com.git.webtable;
 public class GitWebtable {
 public static void main(String[] args) {
 	System.out.println("Welcome to git");
-	int a=10;
 	System.out.println("new line added");
+	int a=10;
 }
 }
